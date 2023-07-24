37d77ddc547bfb9594ae0a87e1a1c9f27b66382e
viniAmorim <amorimm.viniciuss@gmail.com>
2023-07-24 17:08:25 -0300

    Install and configure fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import AppLoading from 'expo-app-loading';
 import { Text, View } from 'react-native';
 
+import {
+  useFonts,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_700Bold,
+  Poppins_800ExtraBold
+} from '@expo-google-fonts/poppins';
+
+import { DMSans_400Regular } from '@expo-google-fonts/dm-sans';
+import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
+
 const App: React.FC = () => {
+  const [fontsLoaded] = useFonts({
+    Poppins_300Light,
+    Poppins_400Regular,
+    Poppins_500Medium,
+    Poppins_700Bold,
+    Poppins_800ExtraBold,
+    DMSans_400Regular,
+    DMSerifDisplay_400Regular
+  })
+
+  if(!fontsLoaded) {
+    return <AppLoading />
+  }
+
   return(
     <View>
       <Text>
-        Wallet App
+        WALLET app
       </Text>
     </View>
   );
