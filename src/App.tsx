682bc33c698cc31a7f8a5440f385ab4d650d781e
viniAmorim <amorimm.viniciuss@gmail.com>
2023-07-24 10:22:23 -0300

    initial configuration

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+const App: React.FC = () => {
+  return(
+    <View>
+      <Text>
+        Wallet App
+      </Text>
+    </View>
+  );
+}
+
+export default App;
